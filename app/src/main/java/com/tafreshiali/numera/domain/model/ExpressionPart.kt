d093657051646c54ff3c2b16096bdfb8029c8813
alitafreshi <alitafreshi123@yahoo.com>
2025-04-29 21:15:48 +0300

    implement expression parser

--- app/src/main/java/com/tafreshiali/numera/domain/model/ExpressionPart.kt
+++ app/src/main/java/com/tafreshiali/numera/domain/model/ExpressionPart.kt
@@ -1,8 +1,7 @@
 package com.tafreshiali.numera.domain.model
 
 sealed interface ExpressionPart {
-    data class Number(val value: Double) : ExpressionPart
-    data class Op(val value: Operation) : ExpressionPart
-    data class Parentheses(val value: ParenthesesType) : ExpressionPart
-
+    data class Number(val number: Double) : ExpressionPart
+    data class Op(val number: Operation) : ExpressionPart
+    data class Parentheses(val type: ParenthesesType) : ExpressionPart
 }
--- app/src/main/java/com/tafreshiali/numera/domain/model/Operation.kt
+++ app/src/main/java/com/tafreshiali/numera/domain/model/Operation.kt
@@ -1,11 +1,11 @@
 package com.tafreshiali.numera.domain.model
 
-enum class Operation(val symbol: Char, val precedence: Int) {
-    ADD(symbol = '+', precedence = 1),
-    SUBTRACT(symbol = '-', precedence = 1),
-    MULTIPLY(symbol = 'x', precedence = 2),
-    DIVIDE(symbol = '/', precedence = 2),
-    PERCENT(symbol = '%', precedence = 2)
+enum class Operation(val symbol: Char) {
+    ADD(symbol = '+'),
+    SUBTRACT(symbol = '-'),
+    MULTIPLY(symbol = 'x'),
+    DIVIDE(symbol = '/'),
+    PERCENT(symbol = '%')
 }
 
 val operationsSymbols = Operation.entries.map { it.symbol }.joinToString("")
